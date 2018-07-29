1967e515fdfda3f6e113a7b7f461f004a5ddbf58
Vincent Boutour <bob@vibioh.fr>
2018-07-29 18:48:23 +0200

    Adding contact block

---
+++ src/js/map.js
@@ -0,0 +1,17 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidmliaW9oIiwiYSI6ImNqMzFmbTFoOTAwMWMycW82YXppbGRidXAifQ.bb6xyGsr1kQW3UozHS2Pmw';
+
+var coordinates = [2.956476579635819, 48.86757827445496];
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: coordinates,
+    zoom: 11
+});
+
+var nav = new mapboxgl.NavigationControl({ showCompass: false });
+map.addControl(nav, 'top-right');
+
+var marker = new mapboxgl.Marker()
+    .setLngLat(coordinates)
+    .addTo(map);
