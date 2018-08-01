0b6719e2ccb82b9cfa36eff259b2644ee5ec78d5
Vincent Boutour <vboutour@meilleursagents.com>
2018-08-01 08:39:02 +0200

    Adding scroll listener for sticky nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,37 +1,68 @@
 NodeList.prototype.__proto__ = Array.prototype;
 
+var stickyNavClass = 'sticky-nav';
 var linkActiveClass = 'nav__link--active';
 
 var nav = document.querySelector('.nav');
+var body = document.querySelector('body');
+
 if (nav) {
   var navHeight = nav.offsetHeight;
 
-  window.addEventListener('resize', function() {
+  /**
+   * Update nav height.
+   */
+  function updateNavHeight() {
     navHeight = nav.offsetHeight;
-  });
+  }
+
+  window.addEventListener('resize', updateNavHeight);
 }
 
-function addActiveModifier(el) {
+if (body && nav) {
+  /**
+   * Update sticky nav state of the body.
+   */
+  function updateStickyState() {
+    var bodyHasStickyClass = body.classList.contains(stickyNavClass);
+
+    if (window.scrollY > navHeight && !bodyHasStickyClass) {
+      body.classList.add(stickyNavClass);
+    } else if (bodyHasStickyClass) {
+      body.classList.remove(stickyNavClass);
+    }
+  }
+
+  window.addEventListener('scroll', updateStickyState);
+}
+
+/**
+ * Set given element to the active nav link.
+ * @param {DOMElement} el Wanted active nav link
+ */
+function setActiveNavLink(el) {
   var active = document.querySelector('.' + linkActiveClass);
   if (active) {
     active.classList.remove(linkActiveClass);
   }
 
-  el.classList.add(linkActiveClass);
+  if (!el.classList.contains(linkActiveClass)) {
+    el.classList.add(linkActiveClass);
+  }
 }
 
 var links = document.querySelectorAll('.nav__link');
 if (links) {
   links.forEach(function(link) {
     link.addEventListener('click', function() {
-      addActiveModifier(link);
+      setActiveNavLink(link);
     });
   });
 }
 
-var currentHash = document.location.hash;
-if (currentHash) {
-  addActiveModifier(document.querySelector('.nav__link[href*="' + currentHash + '"]'));
+var hashLink = document.querySelector('.nav__link[href*="' + document.location.hash + '"]');
+if (hashLink) {
+  setActiveNavLink(hashLink);
 } else {
-  addActiveModifier(links[0]);
+  setActiveNavLink(links[0]);
 }
