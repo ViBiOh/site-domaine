75c58a49d4ad53b8c63c10582dc987309bfacf47
Vincent Boutour <bob@vibioh.fr>
2018-08-02 21:56:39 +0200

    Adding sticky nav with weird behavior of anchor

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,15 +5,32 @@ var linkActiveClass = 'nav__link--active';
 
 var nav = document.querySelector('.nav');
 var body = document.querySelector('body');
+var firstSection = document.querySelector('section');
+
+if (firstSection) {
+  var firstSectionMarginTop = Number(window.getComputedStyle(firstSection).getPropertyValue('margin-top').replace('px', ''));
+}
 
 if (nav) {
   var navHeight = nav.offsetHeight;
+}
 
+if (body && nav && firstSection) {
+  function setBodyMarginTopToNavHeight() {
+    body.style.marginTop = navHeight + firstSectionMarginTop + 'px';
+  }
+}
+
+if (nav) {
   /**
    * Update nav height.
    */
   function updateNavHeight() {
     navHeight = nav.offsetHeight;
+
+    if (body.classList.contains(stickyNavClass)) {
+      setBodyMarginTopToNavHeight();
+    }
   }
 
   window.addEventListener('resize', updateNavHeight);
@@ -26,10 +43,14 @@ if (body && nav) {
   function updateStickyState() {
     var bodyHasStickyClass = body.classList.contains(stickyNavClass);
 
-    if (window.scrollY > navHeight && !bodyHasStickyClass) {
-      body.classList.add(stickyNavClass);
+    if (window.scrollY > navHeight) {
+      if (!bodyHasStickyClass) {
+        body.classList.add(stickyNavClass);
+        setBodyMarginTopToNavHeight();
+      }
     } else if (bodyHasStickyClass) {
       body.classList.remove(stickyNavClass);
+      body.style.marginTop = 0;
     }
   }
 
