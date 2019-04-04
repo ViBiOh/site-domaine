aec725622b7b8fdba1c715c637f628b4103281db
Vincent Boutour <bob@vibioh.fr>
2019-04-04 22:46:49 +0200

    Shortening bash option declaration and removing extra declaration

--- cache.sh
+++ cache.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
-readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+set -o nounset -o pipefail -o errexit
 
 function sed_inplace() {
   if [[ $(uname -s) == "Darwin" ]]; then
